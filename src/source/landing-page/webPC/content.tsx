385359053059181125d036c31e5581ec435f10a4
OscarTo <oscar.to@developertools.com>
2024-06-25 16:52:11 +0900

    fix: update UI

--- src/source/landing-page/webPC/content.tsx
+++ src/source/landing-page/webPC/content.tsx
@@ -1,11 +1,9 @@
-import { SearchComponent } from "../../../share/search";
 import { ProductComponent } from "./products";
 import { Content } from "antd/es/layout/layout";
 
 export const ContentComponent = () => {
   return (
     <Content className="bg-white">
-      <SearchComponent />
       <ProductComponent />
     </Content>
   );
--- src/source/landing-page/webPC/header.tsx
+++ src/source/landing-page/webPC/header.tsx
@@ -1,67 +1,54 @@
-import { Button, Flex, Image, Menu, Row } from "antd";
+import { Button, Flex, Image, Input } from "antd";
 import { Header } from "antd/es/layout/layout";
 
 import logoImage from "../../../assets/logo.jpg";
 import { UserOutlined } from "@ant-design/icons";
 import { useState } from "react";
 import { LoginComponent } from "./login";
+import { useMediaQuery } from "react-responsive";
+import { SearchComponent } from "../../../share/search";
+
+const { Search } = Input;
 
 export const HeaderComponent = () => {
   const [showLogin, setShowLogin] = useState(false);
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
 
-  const menus = [
-    {
-      key: "1",
-      label: "Trang Chủ",
-    },
-    {
-      key: "2",
-      label: "Sản phẩm",
-    },
-    {
-      key: "3",
-      label: "Liên hệ",
-    },
-  ];
   return (
-    <Header
-      style={{
-        display: "block",
-        alignItems: "center",
-        position: "sticky",
-        zIndex: 1,
-        width: "100%",
-        top: 0,
-        backgroundColor: "white",
-        padding: 0,
-      }}
-    >
-      <Row className="w-full justify-between items-center">
-        <Flex>
-          <Image src={logoImage} width={100} height={60} preview={false} />
-          <p className="text-[28px] font-semibold text-blue-700">
-            THỔ KIM <span className="text-yellow-500"> LAND </span>
-          </p>
+    <Header className="block fixed z-50 w-full bg-white p-0 t-0 m-0">
+      <div className="bg-white">
+        <Flex className="w-full justify-between items-center" wrap>
+          <Flex>
+            <Image src={logoImage} width={100} height={60} preview={false} />
+            <p className="text-[28px] font-semibold text-blue-700">
+              THỔ KIM <span className="text-yellow-500"> LAND </span>
+            </p>
+          </Flex>
+          <Flex className="px-2">
+            <Button
+              type="primary"
+              icon={<UserOutlined />}
+              onClick={() => setShowLogin(true)}
+            >
+              {isDesktopOrLaptop ? "Đăng nhập" : ""}
+            </Button>
+          </Flex>
         </Flex>
-        <Flex>
-          <Menu
-            direction="rtl"
-            mode="horizontal"
-            defaultSelectedKeys={["1"]}
-            items={menus}
-            style={{ flex: 1, minWidth: 0 }}
-          />
+        <Flex className="p-2">
+          {isDesktopOrLaptop ? (
+            <SearchComponent />
+          ) : (
+            <Search
+              placeholder="input search text"
+              allowClear
+              className="w-full m-0 p-0"
+              style={{ width: "100%" }}
+            />
+          )}
         </Flex>
-        <Flex gap={8}>
-          <Button
-            type="primary"
-            icon={<UserOutlined />}
-            onClick={() => setShowLogin(true)}
-          >
-            Đăng nhập
-          </Button>
-        </Flex>
-      </Row>
+      </div>
       {showLogin && (
         <LoginComponent open={showLogin} onClose={() => setShowLogin(false)} />
       )}
